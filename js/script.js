81771ebdd15cfb4bc0401fa5df230ed9542eb73a
CosimoTramonte <cosimotramonte7@gmail.com>
2023-03-23 18:15:24 +0100

    fix game and add button restart

--- js/script.js
+++ js/script.js
@@ -27,42 +27,43 @@ btninvia.addEventListener("click", function(){
         document.getElementById("outputEmail").classList.add("true");
         document.getElementById("outputEmail").innerHTML = message;
         document.getElementById("diceTable").classList.remove("hide");
-
-        let diceComputerNumber = Math.floor(Math.random() * 7)
-        let diceUserNumber = Math.floor(Math.random() * 7)
-
-        btnstart.addEventListener("click", function(){
-            
-            if(this.click){
-                document.getElementById("diceComputer").innerHTML = diceComputerNumber;
-            }           
-        })
+ 
 
         btnMyDice.addEventListener("click", function(){
+            let diceUserNumber = Math.floor(Math.random() * 7)
             document.getElementById("diceUser").innerHTML = diceUserNumber;
+
+            let diceComputerNumber = Math.floor(Math.random() * 7)
+            document.getElementById("diceComputer").innerHTML = diceComputerNumber;
 
             if(diceUserNumber > diceComputerNumber){
                 let message = ` 
                 Complimenti, hai vinto!
                 ` 
                 document.getElementById("result").innerHTML = message
-                document.getElementById("result").classList.add("true")
+                document.getElementById("result").classList = "true"
             } else if(diceUserNumber < diceComputerNumber){
                 let message = ` 
                 Mi dispiace, hai perso!
                 ` 
                 document.getElementById("result").innerHTML = message
-                document.getElementById("result").classList.add("wrong")
+                document.getElementById("result").classList = "wrong"
             } else if (diceUserNumber = diceComputerNumber){
                 let message = ` 
                 Pareggio, riprova!
                 ` 
                 document.getElementById("result").innerHTML = message
+                document.getElementById("result").classList = "pareggio"
             }
         })
 
+
+
+
         btnrestart.addEventListener("click", function(){
-            
+            document.getElementById("result").innerHTML = " ";
+            document.getElementById("diceUser").innerHTML = " ";
+            document.getElementById("diceComputer").innerHTML = " ";
         })
 
     
